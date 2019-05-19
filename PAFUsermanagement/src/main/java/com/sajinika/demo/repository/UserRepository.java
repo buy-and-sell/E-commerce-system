4074116de6c796d9c227565eda4db9682b876f7b
Nethuky Kavindi <44235879+SajinikaGallage@users.noreply.github.com>
2019-05-19 18:17:36 +0530

    Add files via upload

---
+++ PAFUsermanagement/src/main/java/com/sajinika/demo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.sajinika.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sajinika.demo.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByUsername(String username);
+	
+}
